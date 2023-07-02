4bd35773a44ef654f513f94f7831bff0602e250d
Alon Fine <mecheniy5462@gmail.com>
2023-07-02 03:12:45 +0300

    made Header+Menu+Profile with using grid

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img alt='kartinka' src="https://cdn.dribbble.com/userupload/8221147/file/original-26ba49bbf930577e669aa44086a1345e.png?compress=1&resize=400x300&vertical=center"></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <body className='content'>
+            <div>
+                <img alt='Emage' src="https://cdn.dribbble.com/userupload/8221626/file/original-13466c8aa976555e1dfe0dd947239391.jpg?compress=1&resize=752x" />
+            </div>
+            <div>
+                ava + decscr
+            </div>
+            <div>
+                my-post
+                <div>
+                    new-post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </body>
+    )
+}
+
+export default Profile;
